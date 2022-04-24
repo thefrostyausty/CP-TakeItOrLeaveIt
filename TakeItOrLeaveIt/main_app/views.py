df12d37bd4488caffda2a13ec22c4b62e420a556
thefrostyausty <austinrobinson0000@gmail.com>
2022-04-24 19:53:47 -0400

    Initial Rendering Setup

--- TakeItOrLeaveIt/main_app/views.py
+++ TakeItOrLeaveIt/main_app/views.py
@@ -1,3 +1,10 @@
+from curses.ascii import HT
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return HttpResponse('<h1>I am Austin the developer</h1>')
