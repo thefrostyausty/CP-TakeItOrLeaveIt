3de1e57dab392204e35c684342057bbe309340dd
thefrostyausty <austinrobinson0000@gmail.com>
2022-04-24 21:50:54 -0400

    basic rendering for sample dataset

--- TakeItOrLeaveIt/main_app/urls.py
+++ TakeItOrLeaveIt/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('events/', views.events_index, name='events_index')
 ]
--- TakeItOrLeaveIt/main_app/views.py
+++ TakeItOrLeaveIt/main_app/views.py
@@ -1,10 +1,30 @@
 from curses.ascii import HT
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+# def about(request):
+#     return HttpResponse('<h1>I am Austin the developer</h1>')
+
 def index(request):
-    return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse('<h1>I am Austin the developer</h1>')
+    return render(request, 'about.html')
+
+class Event:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+events = [
+    Event('Oscars Slap', 'Will Smith in the heat of the moment, attacks Chris Rock'),
+    Event('Kanye Interrupts Taylor Swift', 'Kanye explains to everyone that Beyoncé had the best video of all time'),
+    Event("'Say My Name' music video debuts with a surprise", "The newest music video released by Destiny's Child showed two new members to the surprise of many")
+]
+
+def events_index(request):
+    return render(request, 'events/index.html', {'events': events})
