1b39e92aebd3ff52bb737bbabe71124c87c9c6ce
thefrostyausty <austinrobinson0000@gmail.com>
2022-04-26 16:30:24 -0400

    Full CRUD for Events

--- TakeItOrLeaveIt/main_app/urls.py
+++ TakeItOrLeaveIt/main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('events/', views.events_index, name='events_index'),
-    path('events/<int:event_id>/', views.events_show, name='events_show')
+    path('events/<int:event_id>/', views.events_show, name='events_show'),
+    path('events/create/', views.EventCreate.as_view(), name='events_create'),
+    path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
+    path('events/<int:pk>/delete/', views.EventDelete.as_view(), name='events_delete'),
 ]
--- TakeItOrLeaveIt/main_app/views.py
+++ TakeItOrLeaveIt/main_app/views.py
@@ -1,5 +1,9 @@
+from curses.ascii import CR
+from sre_constants import SUCCESS
 from django.shortcuts import render
 from .models import Event
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -34,3 +38,22 @@ def events_index(request):
 def events_show(request, event_id):
     event = Event.objects.get(id=event_id)
     return render(request, 'events/show.html', {'event': event})
+
+class EventCreate(CreateView):
+    model = Event
+    fields = '__all__'
+    success_url = '/events'
+
+
+class EventUpdate(UpdateView):
+    model = Event
+    fields = ['image', 'title', 'description']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/events/' + str(self.object.pk))
+
+class EventDelete(DeleteView):
+    model = Event
+    success_url = '/events'
